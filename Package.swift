91b51652267dd5dc3ba23a3b352b88af4d09378e
Ferenc Viasz-Kadi <mail.viaszkadi@gmail.com>
2022-05-06 17:46:43 +0200

    Seeking functionality

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "BeyondWords",
             url: "https://github.com/SpeechKit/speechkit-ios-sdk-public/blob/master/BeyondWords.xcframework.zip",
-            checksum: "f2c94da2290c7f1257d2ae5bf5af400cb845072d7c09f10e440266b5b372c5e6"),
+            checksum: "69001f0fdb6236fe67cae70e0bde7eba223b908351c60f778d1768822f555f12"),
         .binaryTarget(
             name: "BeyondWords",
             path: "BeyondWords.xcframework"
